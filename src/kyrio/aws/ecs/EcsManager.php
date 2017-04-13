7bbb5c1fb0cbcb8093aa11e39970a2ddb0592a5b
Santhosh Kulasekar <santhosh.mk@gmail.com>
2017-04-13 00:08:51 -0400

    QA-Cluster func updated

--- src/kyrio/aws/ecs/EcsManager.php
+++ src/kyrio/aws/ecs/EcsManager.php
@@ -103,7 +103,7 @@ class EcsManager
 
     public function getQACluster()
     {
-        return array($this->cluster['arn:aws:ecs:us-east-1:565703495208:cluster/qa-cluster']);
+        return array($this->clusters['arn:aws:ecs:us-east-1:565703495208:cluster/qa-cluster']);
     }
 
     public function getProdCluster()
